26da9b7e603364fe60c3b0996b8ecaca07334544
JeanBrillant <ramahatombojeanbrillant@gmail.com>
2026-07-12 00:10:20 +0300

    Separation des criteres de validation dans d'autre fichier

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCompteRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validated = $request->validate([
-            'name' => 'required',
-            'num_phone' => 'required|min:10|max:10|unique',
-            'firtname' => 'nullable',
-            'password' => 'required|min:6',
-            'password_confirmation' => 'required',
-        ]);
+    public function register(StoreCompteRequest $request){
+        $validated = $request->validated();
 
         $user = User::create($validated);
 
---
+++ app/Http/Requests/StoreCompteRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'num_phone' => 'required|min:10|max:10|unique',
+            'firtname' => 'nullable',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required',
+        ];
+    }
+}
