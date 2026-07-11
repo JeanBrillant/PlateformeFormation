5db795648588dede401e260fcc8fa69d2d9f954e
JeanBrillant <ramahatombojeanbrillant@gmail.com>
2026-07-12 00:05:42 +0300

    Teste sur la creation d'un compte en utilisant l'outil tinker de laravel

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validated = $request->validate([
+            'name' => 'required',
+            'num_phone' => 'required|min:10|max:10|unique',
+            'firtname' => 'nullable',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required',
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'data' => $user
+        ], 201);
+    }
+
+    public function getAllUsers(Request $request){
+        $users = User::all();
+
+        return response()->json([
+            'data' => $users
+        ], 200);
+    } 
 }
