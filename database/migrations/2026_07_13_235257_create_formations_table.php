bd0af9777d57e0bc56d4597729cc22baa7f9258d
JeanBrillant <ramahatombojeanbrillant@gmail.com>
2026-07-14 02:58:45 +0300

    Creation de la migration pour la table des formations

---
+++ database/migrations/2026_07_13_235257_create_formations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('formations', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('description')->nullable();
+            $table->foreignId('cente_id')->constrained()->cascadeOnDelete();
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->time('heure_debut')->nullable();
+            $table->integer('nombre_place')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('formations');
+    }
+};
