48249454d9f1a8334a067db7e3afde26804ee63c
JeanBrillant <ramahatombojeanbrillant@gmail.com>
2026-07-14 01:06:36 +0300

    Condition de validation pour la creation des Centres

---
+++ app/Http/Requests/StoreCentreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCentreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nom_centre' => 'required',
+            'ville_centre' => 'required',
+            'quartier_centre' => 'nullable',
+            'latitude' => 'nullable|numeric|between:-90,90',
+            'longitude' => 'nullable|numeric|between:-180,180',
+        ];
+    }
+}
